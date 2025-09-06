0a05cad813b9079757b10a9d4a39e10d4c3e367b
Divya Adepu <adepudivya96@gmail.com>
2025-09-06 21:19:25 +0530

    Update PersonService.java

--- SpringBoot-REST-JPA (Basic)/src/main/java/com/springboot/person/PersonService.java
+++ SpringBoot-REST-JPA (Basic)/src/main/java/com/springboot/person/PersonService.java
@@ -46,5 +46,53 @@ public class PersonService {
 		
 		personRepo.deleteById(id);
 	}
-	
+	public static void validate(){
+        String a = new String("hello");
+        String b = new String("hello");
+        if (a == b) { 
+            System.out.println("Equal");
+        }
+
+        String input = null;
+        if (input.equals("yes")) { 
+            System.out.println("User said yes");
+        }
+
+        String result = "";
+        for (int i = 0; i < 1000; i++) {
+            result += i; 
+        }
+        System.out.println(result);
+
+        String date = "2025-09-06";
+        String day = date.substring(8, 10); 
+        System.out.println("Day: " + day);
+
+        String csv = "a|b|c";
+        String[] parts = csv.split("|"); 
+        System.out.println("Parts: " + Arrays.toString(parts));
+
+        String userInput = " hello ";
+        if (userInput.equals("hello")) { 
+            System.out.println("Match");
+        }
+
+        String command = "Exit";
+        if (command.equals("exit")) { 
+            System.out.println("Exiting...");
+        }
+
+        String userName = "admin' OR '1'='1";
+        String query = "SELECT * FROM users WHERE name = '" + userName + "';"; 
+        System.out.println("Query: " + query);
+
+        String s = "Hi";
+        char c = s.charAt(5);
+        System.out.println("Char: " + c);
+
+        String text = "1+1=2";
+        String replaced = text.replaceAll("+", "-"); 
+        System.out.println(replaced);
+	}
 }
+
