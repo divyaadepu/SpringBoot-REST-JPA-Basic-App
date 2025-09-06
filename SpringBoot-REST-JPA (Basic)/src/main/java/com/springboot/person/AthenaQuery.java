0a682abf95d750678828bbd5f86e3198c773fb87
Divya Adepu <adepudivya96@gmail.com>
2025-09-06 21:15:01 +0530

    Added Athena Query

---
+++ SpringBoot-REST-JPA (Basic)/src/main/java/com/springboot/person/AthenaQuery.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.util.Map;
+
+public class AthenaQuery {
+
+    private String table;
+    private Map<String, String> queryParams;
+
+    public AthenaRequest(String table, Map<String, String> queryParams) {
+        this.table = table;
+        this.queryParams = queryParams;
+    }
+
+    public String buildQuery() {
+        String query = "SELECT * FROM " + table;
+        if (!queryParams.isEmpty()) {  
+            query = query + " WHERE ";
+            for (Map.Entry<String, String> entry : queryParams.entrySet()) {
+                query = query + entry.getKey() + " = '" + entry.getValue() + "' AND ";  
+            }
+            query = query.substring(0, query.length() - 5);  
+        }
+        return query;
+    }
+
+    public void printQuery() {
+        System.out.println(buildQuery());  
+    }
+}
